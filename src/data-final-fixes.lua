a0d05edd2ff7117e108070c5ec14edd7eb3ae70e
raiguard <caleb.heuer@gmail.com>
2021-06-06 16:30:10 -0600

    Show indicators on mining drills

--- src/data-final-fixes.lua
+++ src/data-final-fixes.lua
@@ -20,42 +20,65 @@ local crafters = {
 local enable_glow = settings.startup["bnl-glow"].value
 local size = constants.sizes[settings.startup["bnl-indicator-size"].value]
 
+local function add_indicator(prototype, wv_root)
+  wv_root = wv_root or prototype
+
+  -- Set status colors
+  wv_root.status_colors = status_colors
+
+  -- Calculate shift for the indicator
+  local selection_box = prototype.selection_box
+  local left_top = selection_box[1]
+  local right_bottom = selection_box[2]
+  local x = left_top[1] + (math.abs(right_bottom[1] - left_top[1]) * constants.horizontal_position)
+  local y = right_bottom[2] - size - constants.additional_vertical_offset
+
+  -- Get or create working visualisations table
+  local wv = wv_root.working_visualisations
+  if not wv then
+    wv = {}
+    wv_root.working_visualisations = wv
+  end
+
+  -- Add to working visualisations
+  wv[#wv + 1] = {
+    always_draw = true,
+    apply_tint = "status",
+    draw_as_light = enable_glow,
+    draw_as_sprite = true,
+    render_layer = "light-effect",
+    animation = {
+      filename = "__BottleneckLite__/graphics/solid.png",
+      size = 64,
+      scale = size,
+      line_length = 1,
+      frame_count = 1,
+      shift = {x, y},
+      animation_speed = 1,
+      direction_count = 1,
+    },
+  }
+end
+
 for _, type in pairs(crafters) do
   for _, crafter in pairs(data.raw[type]) do
-    -- Set status colors
-    crafter.status_colors = status_colors
+    add_indicator(crafter)
+  end
+end
 
-    -- Calculate shift for the indicator
-    local selection_box = crafter.selection_box
-    local left_top = selection_box[1]
-    local right_bottom = selection_box[2]
-    local x = left_top[1] + (math.abs(right_bottom[1] - left_top[1]) * constants.horizontal_position)
-    local y = right_bottom[2] - size - constants.additional_vertical_offset
+for _, drill in pairs(data.raw["mining-drill"]) do
+  drill.status_colors = status_colors
 
-    -- Get or create working visualisations table
-    local wv = crafter.working_visualisations
-    if not wv then
-      wv = {}
-      crafter.working_visualisations = wv
-    end
-
-    -- Add to working visualisations
-    wv[#wv + 1] = {
-      always_draw = true,
-      apply_tint = "status",
-      draw_as_light = enable_glow,
-      draw_as_sprite = true,
-      render_layer = "light-effect",
-      animation = {
-        filename = "__BottleneckLite__/graphics/solid.png",
-        size = 64,
-        scale = size,
-        line_length = 1,
-        frame_count = 1,
-        shift = {x, y},
-        animation_speed = 1,
-        direction_count = 1,
-      },
+  -- Ensure the drill has a graphics set
+  if not drill.graphics_set then
+    drill.graphics_set = {
+      animation = drill.animations
     }
+  end
+
+  add_indicator(drill, drill.graphics_set)
+
+  if drill.wet_mining_graphics_set then
+    add_indicator(drill, drill.wet_mining_graphics_set)
   end
 end
