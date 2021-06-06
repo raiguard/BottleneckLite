1e88030dacc350fcf2ac311f2afaa7c6aa78a044
raiguard <caleb.heuer@gmail.com>
2021-06-06 15:47:11 -0600

    Consolidate code

--- src/data-final-fixes.lua
+++ src/data-final-fixes.lua
@@ -20,36 +20,31 @@ local crafters = {
 local enable_glow = settings.startup["bnl-glow"].value
 local size = constants.sizes[settings.startup["bnl-indicator-size"].value]
 
-local function create_wv(root)
-  local wv = root.working_visualisations
-  if not wv then
-    wv = {}
-    root.working_visualisations = wv
-  end
-  local new_wv = {
-    always_draw = true,
-    apply_tint = "status",
-    draw_as_light = enable_glow,
-    draw_as_sprite = true,
-    render_layer = "light-effect",
-    animation = {
-      filename = "__BottleneckLite__/graphics/solid.png",
-      size = 64,
-      scale = size,
-      line_length = 1,
-      frame_count = 1,
-      -- FIXME: Calculate offset based on entity size
-      shift = {-0.6, 1.3},
-      animation_speed = 1,
-      direction_count = 1,
-    },
-  }
-  wv[#wv + 1] = new_wv
-end
-
 for _, type in pairs(crafters) do
   for _, crafter in pairs(data.raw[type]) do
     crafter.status_colors = status_colors
-    create_wv(crafter)
+    local wv = crafter.working_visualisations
+    if not wv then
+      wv = {}
+      crafter.working_visualisations = wv
+    end
+    wv[#wv + 1] = {
+      always_draw = true,
+      apply_tint = "status",
+      draw_as_light = enable_glow,
+      draw_as_sprite = true,
+      render_layer = "light-effect",
+      animation = {
+        filename = "__BottleneckLite__/graphics/solid.png",
+        size = 64,
+        scale = size,
+        line_length = 1,
+        frame_count = 1,
+        -- FIXME: Calculate offset based on entity size
+        shift = {-0.6, 1.3},
+        animation_speed = 1,
+        direction_count = 1,
+      },
+    }
   end
 end
