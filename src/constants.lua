0af299b675e1f28a4140ec56aa057793e7cb5ee2
raiguard <caleb.heuer@gmail.com>
2021-06-06 14:31:05 -0600

    Generate color and icon settings

--- src/constants.lua
+++ src/constants.lua
@@ -1,37 +1,78 @@
 local constants = {}
 
+constants.colors = {
+  blue = {b = 1},
+  green = {g = 1},
+  purple = {r = 1, b = 1},
+  red = {r = 1},
+  teal = {g = 1, b = 1},
+  white = {r = 1, g = 1, b = 1},
+  yellow = {r = 1, g = 1},
+}
+
+-- TODO: Make a flib function for this usecase
+local color_settings = {}
+for name in pairs(constants.colors) do
+  color_settings[#color_settings + 1] = name
+end
+constants.color_settings = color_settings
+
+constants.icons = {
+  alert = "__BottleneckLite__/graphics/alert.png",
+  cross = "__BottleneckLite__/graphics/cross.png",
+  minus = "__BottleneckLite__/graphics/minus.png",
+  off = "__BottleneckLite__/graphics/off.png",
+  pause = "__BottleneckLite__/graphics/pause.png",
+  solid = "__BottleneckLite__/graphics/solid.png",
+  stop = "__BottleneckLite__/graphics/stop.png",
+}
+
+local icon_settings = {}
+for name in pairs(constants.icons) do
+  icon_settings[#icon_settings + 1] = name
+end
+constants.icon_settings = icon_settings
+
 constants.status_defaults = {
   disabled = {
-    color = {r = 1, g = 1, b = 1},
-    icon = "solid",
+    color = "white",
+    icon = "cross",
+    order = "b",
   },
   full_output = {
-    color = {r = 1, g = 1},
+    color = "yellow",
     icon = "solid",
+    order = "c",
   },
   idle = {
-    color = {r = 1, g = 1},
+    color = "yellow",
     icon = "pause",
+    order = "d",
   },
   insufficient_input = {
-    color = {g = 1},
+    color = "green",
     icon = "solid",
+    order = "e",
   },
   low_power = {
-    color = {r = 1, g = 1},
+    color = "yellow",
     icon = "alert",
+    order = "f",
   },
   no_minable_resources = {
-    color = {r = 1},
+    color = "red",
     icon = "cross",
+    order = "g",
   },
   no_power = {
-    color = {r = 1},
+    color = "red",
     icon = "alert",
+    order = "h",
   },
   working = {
-    color = {g = 1},
+    color = "green",
     icon = "solid",
+    order = "i",
   },
 }
 
--- src/settings.lua
+++ src/settings.lua
@@ -1,8 +1,35 @@
+local constants = require("constants")
+
 data:extend{
   {
     type = "bool-setting",
     name = "bnl-enable",
     setting_type = "startup",
-    default_value = true
+    default_value = true,
+    order = "a",
   }
 }
+
+local status_settings = {}
+for name, spec in pairs(constants.status_defaults) do
+  status_settings[#status_settings + 1] = {
+    type = "string-setting",
+    name = "bnl-color-"..name,
+    localised_name = {"mod-setting-name.bnl-color-setting", {"mod-setting-name.bnl-status-"..name}},
+    setting_type = "startup",
+    default_value = spec.color,
+    allowed_values = constants.color_settings,
+    order = spec.order,
+  }
+  status_settings[#status_settings + 1] = {
+    type = "string-setting",
+    name = "bnl-icon-"..name,
+    localised_name = {"mod-setting-name.bnl-icon-setting", {"mod-setting-name.bnl-status-"..name}},
+    setting_type = "startup",
+    default_value = spec.icon,
+    allowed_values = constants.icon_settings,
+    order = spec.order,
+  }
+end
+
+data:extend(status_settings)
