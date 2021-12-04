af534b2a4086d5bcd3331ee75e2ae2ad02bd1d56
raiguard <caleb.heuer@gmail.com>
2021-12-03 20:49:20 -0700

    Use new flib capability

--- src/data-final-fixes.lua
+++ src/data-final-fixes.lua
@@ -18,12 +18,7 @@ local size = constants.sizes[settings.startup["bnl-indicator-size"].value]
 
 local function build_indicator(prototype)
   -- Calculate shift for the indicator
-  local selection_box = prototype.selection_box or prototype.collision_box or prototype.drawing_box
-  -- TODO: Add a flib function for converting from a keyless representation
-  local Box = area.load{
-    left_top = {x = selection_box[1][1], y = selection_box[1][2]},
-    right_bottom = {x = selection_box[2][1], y = selection_box[2][2]}
-  }
+  local Box = area.load(prototype.selection_box or prototype.collision_box or prototype.drawing_box)
   local positions = {
     north_south = {},
     east_west = {},
